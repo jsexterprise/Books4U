718ae28428ef4f8cac77fe0aa8f9b770cb2282d9
jsexterprise <67800926+jsexterprise@users.noreply.github.com>
2020-08-19 18:53:17 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,33 @@
+onEvent("button1", "click", function( ) {
+  setScreen("screen2");
+});
+onEvent("screen3btn", "click", function( ) {
+  setScreen("screen3");
+});
+onEvent("screen1btn", "click", function( ) {
+  setScreen("screen1");
+});
+onEvent("screen2btn", "click", function( ) {
+  setScreen("screen2");
+});
+onEvent("screen4btnfront", "click", function( ) {
+  setScreen("screen4");
+});
+onEvent("screen5btnfront", "click", function( ) {
+  setScreen("screen5");
+});
+onEvent("screen3btnback", "click", function( ) {
+  setScreen("screen3");
+});
+onEvent("screen4btnback", "click", function( ) {
+  setScreen("screen4");
+});
+onEvent("screen6btnfront", "click", function( ) {
+  setScreen("screen6");
+});
+onEvent("screen5btnback", "click", function( ) {
+  setScreen("screen5");
+});
+onEvent("screen6btnfront", "click", function( ) {
+  setScreen("screen6");
+});
